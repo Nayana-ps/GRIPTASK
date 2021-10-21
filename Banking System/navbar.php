d50f1e01c5eebd3161beba15de677d874f2c3548
Nayana-ps <88576924+Nayana-ps@users.noreply.github.com>
2021-10-21 12:20:13 +0530

    Add files via upload

---
+++ Banking System/navbar.php
@@ -0,0 +1,26 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+       <!-- <a class="navbar-brand" href="index.php"></a>  -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+        <link rel="stylesheet" type="text/css" href="css/navbar.css">
+      </button>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              
+              <li class="nav-item"><a>
+                <a class="nav-link" href="index.php"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><b>Transaction History</b></a>
+              </li>
+          </div>
+</a>
+       </nav>
